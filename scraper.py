5cc9c510f32faaa7145eee8ae6e73f1ccc1789d4
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-06-08 16:29:39 +0200

    clean code and add comment

--- scraper.py
+++ scraper.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from urllib import request
 from bs4 import BeautifulSoup
 
+# cleaner fonction for file name
+
 
 def replace_all(text, dic):
     for i, j in dic.items():
@@ -16,7 +18,7 @@ def replace_all(text, dic):
 
 """
 
-récupération des catégories d'ouvrages
+getting all books category
 
 """
 
@@ -28,35 +30,40 @@ if response_all.ok:
 
     soup = BeautifulSoup(response_all.content, 'lxml')
 
+# get all categorys <a> tag with links and names
 category_list = soup.find('ul', class_='nav nav-list').select('a')
 
+# get link from category_list and use it to make the full link to category
 links = ["http://books.toscrape.com/catalogue/category" +
          el.get('href')[2:] for el in category_list[1:]]
 
+# get clean name from caterory list
 names = [" ".join(el.text.split()) for el in category_list[1:]]
 
+# send name and link into a dictionary
 categorys = dict(zip(names, links))
 
 
 """
 
-récupération de tout les ouvrages d'une catégorie
+get every books from  every category
 
 """
 
-# for every category
+# for every categorys take name and link renamed category and category_page_url
 for category, category_page_url in categorys.items():
-
-    Path(f'./ScrapedData/{category}/IMG').mkdir(parents=True, exist_ok=True)
 
     response_cat = requests.get(category_page_url[:-10] + "index.html")
 
+    # if a category have more than one page, link while start at "page-2.html", initialize page_number at 2
     page_number = 2
 
     books_links = []
 
+    # print who help to know where the scraping is at
     print(f'\n {category} \n')
 
+    # get every page
     while response_cat.ok:
         soup = BeautifulSoup(response_cat.content, 'lxml')
 
@@ -65,13 +72,13 @@ for category, category_page_url in categorys.items():
 
         page_number += 1
 
-    # get full url by taking relative link from very book starting index 7 and adding original url on the begining
+    # get full url by taking relative link from every book starting index 8 and adding original url on the begining
         books_links.extend(["http://books.toscrape.com/catalogue" + book_link.get('href')[8:]
                            for book_link in soup.find('ol', class_='row').select('h3 > a')])
 
     """ 
 
-    récupération des données de l'ouvrage
+    get data from a book
 
     """
 
@@ -82,24 +89,28 @@ for category, category_page_url in categorys.items():
         if response.ok:
             soup = BeautifulSoup(response.content, 'lxml')
 
+        # shortcut for others variables
         product_main = soup.find('div', class_='col-sm-6 product_main')
 
         title = product_main.find('h1').text
 
-        cleaner = {",": "", "#": "", " ": "_", "*": "", "?": "", ":": "",
-                   "/": "", "\\": "", "|": "", "<": "", ">": "", "\"": ""}
-
+        # getting image_url befor cleanning title
         image_url = ("http://books.toscrape.com" +
                      soup.find(attrs={'alt': title})['src'][5:])
 
-        # save image from url with the book name without space and the right img format
+        # initialize a dict to use into replace_all function
+        cleaner = {",": "", "#": "", " ": "_", "*": "", "?": "", ":": "",
+                   "/": "", "\\": "", "|": "", "<": "", ">": "", "\"": ""}
         title = "".join(replace_all(title, cleaner).split("_(", 1)[0])
+
+        # save image from url with the book name without space and the right img format
         image = request.urlretrieve(
             image_url, f'./ScrapedData/{category}/IMG/{title}.{image_url.split(".")[-1]}')
 
+        # regex to find number available
         number_available = re.search(
             '\d+', product_main.find('p', class_='instock availability').text)[0]
-
+        # use regex to found every class starting with "star-rating"
         review_rating = product_main.find(
             'p', class_=re.compile('star-rating'))['class'][1]
 
@@ -116,6 +127,7 @@ for category, category_page_url in categorys.items():
         else:
             review_rating = "0"
 
+        # test if correct to avoid error
         product_description = soup.select_one('#product_description + p')
 
         if product_description == None:
@@ -125,6 +137,7 @@ for category, category_page_url in categorys.items():
 
         tablesoup = soup.find('table', class_='table table-striped')
 
+        # init varriable and populate them with the html table
         th = []
         td = []
         for i in tablesoup.find_all('th'):
@@ -132,6 +145,7 @@ for category, category_page_url in categorys.items():
         for i in tablesoup.find_all('td'):
             td.append(i.text)
 
+        # transform it into a dict
         table = dict(zip(th, td))
 
         universal_product_code = table.get('UPC')
@@ -140,13 +154,20 @@ for category, category_page_url in categorys.items():
 
         price_including_tax = table.get('Price (incl. tax)')
 
+        # create dict with all the date using fieldname for the key value
         book_detail = {'product_page_url': str(product_page_url), 'universal_product_code (upc)': str(universal_product_code), 'title': str(title), 'price_including_tax': str(price_including_tax), 'price_excluding_tax': str(
             price_excluding_tax), 'number_available': str(number_available), 'product_description': str(product_description), 'category': str(category), 'review_rating': str(review_rating), 'image_url': str(image_url)}
 
+        fieldnames = ['product_page_url', 'universal_product_code (upc)', 'title', 'price_including_tax',
+                      'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+
+        # verify if directory exist, if not, create it with parent folder
+        Path(
+            f'./ScrapedData/{category}/IMG').mkdir(parents=True, exist_ok=True)
+
+        # verify if CSV exist, if exist, add a new book to it, if not, create a header and add the first book to it.
         if Path(f'ScrapedData/{category}/{category}.csv').is_file():
             with open(f'ScrapedData/{category}/{category}.csv', 'a', encoding="utf-8-sig", newline="") as bk:
-                fieldnames = ['product_page_url', 'universal_product_code (upc)', 'title', 'price_including_tax',
-                              'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
                 reader = csv.DictReader(
                     bk, dialect='excel', delimiter=';', fieldnames=fieldnames)
                 writer = csv.DictWriter(
@@ -154,8 +175,6 @@ for category, category_page_url in categorys.items():
                 writer.writerow(book_detail)
         else:
             with open(f'ScrapedData/{category}/{category}.csv', 'w', encoding="utf-8-sig", newline="") as bk:
-                fieldnames = ['product_page_url', 'universal_product_code (upc)', 'title', 'price_including_tax',
-                              'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
                 reader = csv.DictReader(
                     bk, dialect='excel', delimiter=';', fieldnames=fieldnames)
                 writer = csv.DictWriter(
